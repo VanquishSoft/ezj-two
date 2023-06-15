5866d00303304ef83ef568f0c749dc1ce466af66
VanquishSoft <60197142+VanquishSoft@users.noreply.github.com>
2023-06-15 01:52:21 -0600

    update

--- iframe.js
+++ iframe.js
@@ -1,5 +1,4 @@
 function displayMessage (evt) {
-	document.getElementById("test").innerHTML += "\n data:"+JSON.stringify(evt.data);
 	if (evt.origin === "https://app.jillsoffice.com") {
 		localStorage.setItem("location-data", JSON.stringify(evt.data));
 	}
